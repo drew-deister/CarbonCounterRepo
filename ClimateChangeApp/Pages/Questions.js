402ed4a47c9aec00bde1077e62d4fcfcac4e29d4
Ethan Shifrin <ethan.shifrin@vanderbilt.edu>
2019-12-19 16:45:55 -0600

    Ethan's first rendition of Question and Housing component

--- ClimateChangeApp/Pages/Questions.js
+++ ClimateChangeApp/Pages/Questions.js
@@ -1,13 +1,47 @@
-import React, { Component } from 'react';
+// Housing.js
+// Author: Ethan Shifrin and Drew Deister
+// Email: Ethan.Shifrin@Vanderbilt.edu
+// Date: 12/19/19
+// parent files: This Questions page is the first page of the Carbon Counter, accessed from the home page (../App.js)
+// purpose: Display the Housing* survey, the first of the Carbon Counter survey page.
+//          
+// To Do:
+//    1. Implement state to keep track of user input (see Question component)
+//    2. Learn about inheritance to simplify styles (see Housing)
+//    3. look into stack warning (seen when running the code: "Accessing view manager configs directly off UIManager via UIManager['getConstants']
+//                                                             is no longer supported.
+//                                                             Use UIManager.getViewManagerConfig('getConstants') instead"
+//    4. Fill out page with title, progress bar, further navigation
+
+
+import React, { Component, useState } from 'react';
 import {StyleSheet, View} from "react-native";
 import {Button, Text, Card, Icon} from 'react-native-elements';
+import { TextInput } from 'react-native-gesture-handler';
+import { Question } from '../components/Question';
+import { Housing } from '../Surveys/Housing'
+
 
 class Questions extends React.Component {
     render() {
         return(
-            <Text>Hello!</Text>
+          <View style={styles.housingScreen}>
+            <Housing />
+          </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+  // The housing screen currently holds the housingContainer (aka Housing component)
+  // it will eventually hold the progress bar and potentially the navigation features
+  housingScreen: {
+    paddingTop: 50,
+    paddingHorizontal: 5,
+    paddingBottom: 5,
+    height: '100%',
+    //backgroundColor: 'black'
+  },
+});
 
 export default Questions;
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # CarbonCounterRepo
+
+Date: 12/14/19\
+Branch Author: Ethan Shifrin\
+Branch Purpose: A basic navigation of home screen with a Carbon Counter button has been created.\
+In order to get a handle on our power and capabilities with react native, this branch is my attempt to build question components for the housing page of the Carbon Counter Survey\
+\
+Documentation for every document should look like the following\
+// Question.js\
+// Author: Ethan Shifrin\
+// Email: Ethan.Shifrin@Vanderbilt.edu\
+// Date: 12/19/19\
+// parent files: This question component is built with the housing survey in mind (../Surveys/Housing.js)\
+// purpose: Display a single question and text input on a Carbon Counter survey screen.\
+//          For now, it is just used in the Housing component, but hopefully will be extended to other categories\
+//          note: author had difficulty implementing state, therefore state is not yet used\
+// To Do:\
+//    1. Implement state to keep track of user input\
