7d97f314c3db68b8940808682ca7881013b0b8a0
emshifrin <ethan.shifrin@vanderbilt.edu>
2020-01-16 14:25:57 -0600

    cleaned up basic video implementation

--- ClimateChangeApp/Pages/GeoVideo2.js
+++ ClimateChangeApp/Pages/GeoVideo2.js
@@ -6,16 +6,6 @@ import { Video } from 'expo-av';
 //import { WebView } from 'react-native';
 
 const { width: DEVICE_WIDTH, height: DEVICE_HEIGHT } = Dimensions.get("window");
-const BACKGROUND_COLOR = "#FFF8ED";
-const DISABLED_OPACITY = 0.5;
-const FONT_SIZE = 14;
-const LOADING_STRING = "... loading ...";
-const BUFFERING_STRING = "...buffering...";
-const RATE_SCALE = 3.0;
-const VIDEO_CONTAINER_HEIGHT = (DEVICE_HEIGHT * 2.0) / 5.0 - FONT_SIZE * 2;
-
-const LOOPING_TYPE_ALL = 0;
-const LOOPING_TYPE_ONE = 1;
 
 class GeoVideo2 extends React.Component {
     static navigationOptions = { // this is the label in the middle of the nav bar
@@ -26,33 +16,21 @@ class GeoVideo2 extends React.Component {
         const { width } = Dimensions.get('window');
         return(
           <View style={styles.screen}>
-              <View style={styles.VideoContainer}>
-                <Text style={styles.Title}>The non-best game ever played was on a Wednesday in Chicago</Text>
-                <View style={styles.Video}>
-                <View style={styles.videoContainer}>
-                    {/* <WebView
-                        style={{flex:1}}
-                        javaScriptEnabled={true}
-                        source={{html: '<h1>Hello world</h1>'}}
-                    /> */}
-                    <Video 
-                        //popeye uri: 'https://ia800501.us.archive.org/11/items/popeye_i_dont_scare/popeye_i_dont_scare_512kb.mp4'
-                        //youtube uri does not work
-                        source={{ uri: 'https://ia800501.us.archive.org/11/items/popeye_i_dont_scare/popeye_i_dont_scare_512kb.mp4'}}
-                        rate={1.0}
-                        volume={1.0}
-                        isMuted={false}
-                        resizeMode="cover"  //fill container bounds while preserving aspect ratio
-                        useNativeControls={true}
-                        shouldPlay
-                        style={{ width, height: 260}}
-                    />
-
-                </View>
-                {/* <Text style={styles.Video}>The video should play here</Text> */}
-              </View>
-            
-            
+              <View style={styles.MainContainer}>
+                  <Text style={styles.Title}>Popeye</Text>
+                  <Video
+                    //popeye uri: 'https://ia800501.us.archive.org/11/items/popeye_i_dont_scare/popeye_i_dont_scare_512kb.mp4'
+                    source={{ uri: 'https://ia800501.us.archive.org/11/items/popeye_i_dont_scare/popeye_i_dont_scare_512kb.mp4' }}
+                    style={styles.Video}
+                    resizeMode="cover"      //fill container bounds while preserving aspect ratio
+                    shouldPlay
+                    useNativeControls={true}
+                    rate={1.0}
+                    isMute={false}
+                    volume={1.0}
+                  >
+                      <Text style={styles.Caption}>Do you like this video?</Text>
+                    </Video>
             </View>
           </View>
         )
@@ -65,32 +43,46 @@ const styles = StyleSheet.create({
         backgroundColor: '#D0F0C0',
         padding: 10
     },
-    VideoContainer: {
+    MainContainer: {
         //flex: 1,
         flexDirection: "column",
         alignItems: 'center',
         justifyContent: 'space-around',
         padding: 0,
-        height: DEVICE_HEIGHT,
-        width: DEVICE_WIDTH
-        //borderColor: 'black',
-        //borderWidth: 13
+        //height: DEVICE_HEIGHT,
+        //width: DEVICE_WIDTH,
+        borderColor: 'black',
+        borderWidth: 1
       },
     Title: {
         color: 'black',
         fontSize: 20,
-        justifyContent: 'center',
-        alignItems: 'center',
+        //justifyContent: 'center',
+        //alignItems: 'center',
         //flex: 1,
         backgroundColor: 'white',
-        height: '20%'
+        borderColor: 'black',
+        borderWidth: 1,
+        height: '10%',
+        width: '60%',
+        textAlign: 'center',
+        textAlignVertical: 'center'
+    },
+    Caption: {
+        borderColor: 'black',
+        borderWidth: 1,
+        textAlign: 'center',
+        //textAlignVertical: 'bottom'
     },
     Video: {
         //flex: 3,
         backgroundColor: 'white',
-        justifyContent: 'center',
-        width: '100%',
-        height: '60%'
+        //justifyContent: 'center',
+        //width: '100%',
+        borderColor: 'black',
+        borderWidth: 1,
+        height: '55%',
+        width: '90%'
     }
   });
 
