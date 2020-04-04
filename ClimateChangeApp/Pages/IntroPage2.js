967c6298c5885a3f294cd843c1de1329bb8b0725
Asaf <asaf.roth@vanderbilt.edu>
2020-04-04 17:59:11 -0400

    reda

--- ClimateChangeApp/Pages/IntroPage2.js
+++ ClimateChangeApp/Pages/IntroPage2.js
@@ -1,5 +1,11 @@
 // Asaf
 
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+  listenOrientationChange,
+  removeOrientationListener,
+} from "react-native-responsive-screen";
 import React, { Component } from "react";
 //import HomeScreenActivityCard from '../Components/HomeScreenActivityCard';
 import { View, Text, StyleSheet } from "react-native";
@@ -49,20 +55,21 @@ const styles = StyleSheet.create({
     backgroundColor: "#F6F8EF",
     borderColor: "#707070",
     justifyContent: "space-around",
-    alignItems: "center"
+    alignItems: "center",
   },
   buttonDesign: {
     color: "#73A388",
     fontSize: 23,
     fontWeight: "600",
-    margin: 30
+    margin: 30,
   },
   topText: {
     color: "#73A388",
     fontSize: 50,
     fontWeight: "600",
     width: 300,
-    height: 200
+    height: 200,
+    //width percentage and aspect ratio
   },
   bottonText: {
     color: "#73A388",
@@ -70,9 +77,9 @@ const styles = StyleSheet.create({
     fontWeight: "600",
     fontSize: 27,
     width: 300,
-    height: 230
+    height: 230,
   },
   surroundingButton: {
-    paddingTop: 60
-  }
+    paddingTop: 60,
+  },
 });
