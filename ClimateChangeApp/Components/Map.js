b7cfe903203670c2dfe1b035aae249a309a26b45
Asaf <asaf.roth@vanderbilt.edu>
2020-04-10 19:01:21 -0400

    pop up

--- ClimateChangeApp/Components/Map.js
+++ ClimateChangeApp/Components/Map.js
@@ -1,79 +1,75 @@
-import React from 'react';
-import MapView, { Marker } from 'react-native-maps';
-import {StyleSheet, View} from "react-native";
+import React from "react";
+import MapView, { Marker } from "react-native-maps";
+import { StyleSheet, View } from "react-native";
 
-const alps = 
-  {
-    latitude: 46.88,
-    longitude: 9.65,
-    latitudeDelta: 15,
-    longitudeDelta: 15,
-  }
-
+const alps = {
+  latitude: 46.88,
+  longitude: 9.65,
+  latitudeDelta: 15,
+  longitudeDelta: 15,
+};
 
 class Map extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          location: alps,
-        }
-    }
-
-    handleLocationChange = (newLocation) => {
-        this.setState({ location: newLocation });
-        //this.checkLocationsSeen;
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: alps,
     };
+  }
 
-    //state = {  }
-    render() { 
-        return (  
-            <View style={styles.mapContainer}>
-                <MapView 
-                    //style={styles.mapStyle}
-                    //annotations={markers}
-                    mapType='satellite'
-                    customMapStyle={mapstyle}
-                    initialRegion={alps}
-                    region={this.state.location}
-                    onRegionChange={this.handleLocationChange}
-                >
-                  {this.state.markers.map(marker => (
-                    <Marker
-                      key={marker.key}
-                      coordinate={marker.coordinate}
-                      pinColor={marker.color}
-                    />
-                  ))}
+  handleLocationChange = (newLocation) => {
+    this.setState({ location: newLocation });
+    //this.checkLocationsSeen;
+  };
 
-                </MapView>
-            </View>
-        )
-    }
+  //state = {  }
+  render() {
+    return (
+      <View style={styles.mapContainer}>
+        <MapView
+          //style={styles.mapStyle}
+          //annotations={markers}
+          mapType="satellite"
+          customMapStyle={mapstyle}
+          initialRegion={alps}
+          region={this.state.location}
+          onRegionChange={this.handleLocationChange}
+        >
+          {this.state.markers.map((marker) => (
+            <Marker
+              key={marker.key}
+              coordinate={marker.coordinate}
+              pinColor={marker.color}
+            />
+          ))}
+        </MapView>
+      </View>
+    );
+  }
 }
 
 export default Map;
 
 const styles = StyleSheet.create({
-    mainContainer: {
-      flex: 1,
-      flexDirection: "column",
-      alignItems: 'center',
-      justifyContent: 'space-around',
-      padding: 0,
-      //height: DEVICE_HEIGHT,
-      //width: DEVICE_WIDTH,
-      borderColor: 'black',
-      borderWidth: 1
-    },
-    mapContainer: {
-      //flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    mapStyle: {
-      width: 300, //Dimensions.get('window').width,
-      height: 400, //Dimensions.get('window').height,
-    }
-  });
- 
+  mainContainer: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-around",
+    padding: 0,
+    //height: DEVICE_HEIGHT,
+    //width: DEVICE_WIDTH,
+    borderColor: "black",
+    borderWidth: 1,
+  },
+  mapContainer: {
+    //flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  mapStyle: {
+    width: 300, //Dimensions.get('window').width,
+    height: 400, //Dimensions.get('window').height,
+  },
+});
